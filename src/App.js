5af0fcbce54666886ccca6fd5d3123fc15f7742c
https://github.com/Fanel99 <fanelsecara@gmail.com>
2021-12-08 15:47:42 +0100

    fetch data and  pass the prop into component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Create from './pages/create/Create';
+import Home from './pages/home/Home';
+import Recipe from './pages/recipe/Recipe';
+import Search from './pages/search/Search';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/create">
+            <Create />
+          </Route>
+          <Route path="/search">
+            <Search />
+          </Route>
+          <Route path="/recipes/:id">
+            <Recipe />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,5 +1,14 @@
 import './Home.css';
+import RecipeList from '../../components/RecipeList';
+import { useFetch } from '../../hooks/useFetch';
 
 export default function Home() {
-  return <div>Home</div>;
+  const { data, isPending, error } = useFetch(' http://localhost:3000/recipes');
+  return (
+    <div className="home">
+      {error && <p className="error">{error} </p>}
+      {isPending && <p className="loading">{isPending} </p>}
+      {data && <RecipeList recipes={data} />}
+    </div>
+  );
 }
